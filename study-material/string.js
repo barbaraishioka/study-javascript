19970da034b6c11c8a0d5e695623c9f9fa161a2a
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-05 20:45:29 -0300

    content about string

---
+++ study-material/string.js
@@ -0,0 +1,10 @@
+// ----- Tipo de Dado -> String -----
+
+let myFirstString = "Texto"; // pode ser usado aspas duplas ou simples.
+
+let mySecondString = "Marca d'agua"; // pode ser usado aspas simples dentro das aspas duplas ou vice versa, mas tome cuidado.
+
+let variableName;
+let myTemplateString = `Chamado de Template String ou Template Literals, podemos inserir javascript ${variableName} ou cálculos 
+${5 + 5}
+e também podemos pular linhas`;
