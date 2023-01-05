d45147447bdb5806e42783c98c692094e1c759dc
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-05 20:27:49 -0300

    content about variables

---
+++ study-material/variable.js
@@ -0,0 +1,23 @@
+// Variáveis
+
+// var; -> Não usar mais.
+// let;
+// const; -> Constante, ou seja, o valor não muda.
+
+// Regras
+
+// ----- Permitido -----
+// Iniciar com $ ou _
+// Colocar acentos
+// Iniciar com letras
+// Letras maiúsculas e minúsculas
+
+// ----- Proibido -----
+// Iniciar com números
+// Colocar espaços
+
+// ----- Ideal -----
+// Camel ase -> camelCase
+// Snake case -> snake_case
+// Sempre em inglês
+// Nomes que fazem sentido e seja intuitivo
