51e3d56cf3d1e5db0e503c194f59ccbba20246bd
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-05 21:38:55 -0300

    solution for exercise 3~

---
+++ challenges_01/ex3.js
@@ -0,0 +1,8 @@
+// [ ] Faça um programa que encontre a raiz quadrada de um número, multiplique o resultado por 10 e divida por 33
+
+const number = 900;
+const calculateRoot = Math.sqrt(number);
+
+const result = (calculateRoot * 10) / 33;
+
+console.log(result);
