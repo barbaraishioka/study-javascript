14e525d0ff51589d6c791b738c4a4e10754e33da
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-05 21:44:25 -0300

    solution for exercise 17

---
+++ challenges_01/ex17.js
@@ -0,0 +1,7 @@
+// [ ] Crie um programa que mostra o tamanho do Array. Ex: const array = [0, 1] imprime: 2. const array = ["banana", "Maça", "laranja"] imprime: 3
+
+const array = [0, 1];
+const array2 = ["banana", "Maça", "laranja"];
+
+console.log(array.length);
+console.log(array2.length);
