0eed8e7d8f238133421e8180f18a3adfa293e625
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-05 21:41:18 -0300

    solution for exercise 8

---
+++ challenges_01/ex8.js
@@ -0,0 +1,9 @@
+// [ ] Faça um programa que imprima na tela se um nome é diferente ao outro nome digitado. Porém, os dois nomes devem estar em um Array. Ex: const array = ["João", "Maria"]. João e João, imprime false. João e Maria, imprime true.
+
+const array = ["João", "Maria"];
+
+const resultFalse = array[0] !== array[0];
+const resultTrue = array[0] !== array[1];
+
+console.log(resultFalse);
+console.log(resultTrue);
