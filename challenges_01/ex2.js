ed0c9a9290718c3966ac613268d9f338a9822106
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-05 21:38:32 -0300

    solution for exercise 2

---
+++ challenges_01/ex2.js
@@ -0,0 +1,9 @@
+// [ ] Faça um programa que multiplique 2 números e o resultado adicione 10
+
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(2, 2) + 10;
+
+console.log(result);
