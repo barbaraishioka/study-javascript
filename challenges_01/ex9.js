cdb938a8a4364394c0dc70303a12301f229fc8c9
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-05 21:41:38 -0300

    solution for exercise 9

---
+++ challenges_01/ex9.js
@@ -0,0 +1,51 @@
+// [ ] Criei 5 Objetos, neles devem conter os dados de 5 pessoas que você conhece. Mínimo 5 dados por pessoa.
+
+const person1 = {
+  name: "Alex",
+  sex: "male",
+  age: "20",
+  adress: {
+    country: "Brazil",
+    state: "São Paulo",
+  },
+};
+
+const person2 = {
+  name: "Bianca",
+  sex: "female",
+  age: "32",
+  adress: {
+    country: "Itália",
+    state: "Roma",
+  },
+};
+
+const person3 = {
+  name: "Carlos",
+  sex: "male",
+  age: "53",
+  adress: {
+    country: "Japão",
+    state: "Okinawa",
+  },
+};
+
+const person4 = {
+  name: "Dalila",
+  sex: "female",
+  age: "18",
+  adress: {
+    country: "França",
+    state: "Paris",
+  },
+};
+
+const person5 = {
+  name: "Eduardo",
+  sex: "male",
+  age: "43",
+  adress: {
+    country: "Estados Unidos",
+    state: "Nova York",
+  },
+};
