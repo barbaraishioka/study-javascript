6c30ad6e45da9bc1215315e08de98508eab71dff
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-05 21:17:13 -0300

    content about boolean

---
+++ study-material/boolean.js
@@ -0,0 +1,10 @@
+// ----- Tipo de Dado -> Boolean -----
+
+// Só tem dois valores
+// true (1) <-> false (0)
+
+let a = 5;
+let b = 10;
+
+console.log(a == b); // false
+console.log(a == a); // true
