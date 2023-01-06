0bca90d75047c4d0376878b55c5e0bb30110c687
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-05 21:39:41 -0300

    solution for exercise 4

---
+++ challenges_01/ex4.js
@@ -0,0 +1,8 @@
+// [ ] Faça um programa que inicie com dois nomes, e o programa imprima na tela o seguinte dado: Olá, meu nome é (nome 1) e meu partner do CodeClub é (nome2)
+
+const person1 = "João";
+const person2 = "Maria";
+
+const result = `Olá, meu nome é ${person1} e meu partner do CodeClub é ${person2}`;
+
+console.log(result);
