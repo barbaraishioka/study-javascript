2028555f0e52d0ff57b766c1e66c90a5d74fc654
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-05 21:40:06 -0300

    solution for exercise 5

---
+++ challenges_01/ex5.js
@@ -0,0 +1,10 @@
+// [ ] Faça um programa que imprima na tela se um nome é igual ao outro nome digitado. Ex: João e João, imprime true. João e Maria, imprime false.
+
+const person1 = "João";
+const person2 = "Maria";
+
+const resultTrue = person1 == person1;
+const resultFalse = person1 == person2;
+
+console.log(resultTrue);
+console.log(resultFalse);
