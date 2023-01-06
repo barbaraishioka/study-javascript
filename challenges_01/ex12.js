d1be5de3062e43affb79de284632e3ab74c0e97c
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-05 21:42:54 -0300

    solution for exercise 12

---
+++ challenges_01/ex12.js
@@ -0,0 +1,6 @@
+// [ ] Faça um programa onde toda vez que ele rodar, coloca na tela um número aleatório entre 1 e 100.
+
+const random1to100 = Math.random() * 100;
+const result = random1to100;
+
+console.log(result);
