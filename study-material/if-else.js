8b83eff8542a0a0e2a932a789d7cdca29c9b95d3
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-14 11:33:36 -0300

    add content about else-if

--- study-material/if-else.js
+++ study-material/if-else.js
@@ -4,6 +4,7 @@ CONTROLE DE FLUXO - CONDICIONAIS - IF / ELSE
 
 if -> SE
 else -> SENÃO
+else if -> MAIS SE
 
 
 if (condition true) { SE a condição é verdadeira
@@ -14,6 +15,8 @@ if (condition true) { SE a condição é verdadeira
 
 */
 
+// EXEMPLO IF ELSE
+// detalhe: pode ter if sem else, mas não else sem if
 const idade = 15;
 
 if (idade >= 18) {
@@ -23,4 +26,17 @@ if (idade >= 18) {
   console.log("A pessoa é MENOR de idade");
 }
 
-// detalhe: pode ter if sem else, mas não else sem if
+// EXEMPLO ELSE IF
+
+const temperature = 37;
+
+if (temperature >= 36 && temperature <= 37) {
+  // SE
+  console.log("A pessoa está saudável");
+} else if (temperature > 37) {
+  // MAIS SE
+  console.log("A pessoa está com febre");
+} else {
+  // SENÃO
+  console.log("A pessoa está com a temperatura muito baixa");
+}
