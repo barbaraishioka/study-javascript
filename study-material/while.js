63f4b9c54b9ef0e81d62cd62901e4f44f7bddcb2
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-28 09:37:53 -0300

    add content about while

---
+++ study-material/while.js
@@ -0,0 +1,27 @@
+/* 
+ESTRUTURA DE REPETIÇÃO - WHILE
+
+while (condition) {}
+
+Enquanto a condição for verdadeira, executa o código.
+
+*/
+
+let i = 0;
+while (i < 20) {
+  console.log(i);
+  i += 2;
+}
+
+/* RESULTADO
+0
+2
+4
+6
+8
+10
+12
+14
+16
+18
+*/
