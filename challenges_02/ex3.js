e74a13834f59eb2dc18b8b254b4b24c18434f33d
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-28 17:26:05 -0300

    add my solution and teacher's solution

--- challenges_02/ex3.js
+++ challenges_02/ex3.js
@@ -1,1 +1,18 @@
 // [ ] Faça um programa que gere um número aleatório entre 1 e 10, esse número é o número 'ganhador'. Então ele também irá gerar um número aleatório entre 1 e 10, e verifica se a pessoa ganhou ou não. Em caso de ter ganhado, mostra o prêmio, se não ganhou, mostra outra mensagem.
+
+// ***** MINHA SOLUÇÃO *****
+
+const winner = Math.round(Math.random() * (10 - 1) + 1);
+const randomNumber = Math.round(Math.random() * (10 - 1) + 1);
+const isWinner = winner == randomNumber ? "Prêmio!!!" : "Tente novamente";
+console.log(isWinner);
+
+// ***** SOLUÇÃO PROFESSOR *****
+
+const randomNumber2 = Math.floor(Math.random() * (10 - 1) + 1);
+const winningNumber = Math.floor(Math.random() * (10 - 1) + 1);
+if (randomNumber2 === winningNumber) {
+  console.log("Você ganhou");
+} else {
+  console.log("Você perdeu");
+}
