2fba05cdca0d7f0911928f63685a2b077af4f877
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-28 09:23:35 -0300

    add content about for

---
+++ study-material/for.js
@@ -0,0 +1,21 @@
+/* 
+ESTRUTURA DE REPETIÇÃO - FOR
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
+
+*/
+
+const students = ["Bruna", "Anna", "João", "Maria", "Pedro"];
+for (let i = 0; i < students.length; i++) {
+  console.log(`${students[i]} está presente a aula`);
+}
+
+/* RESULTADO
+Bruna está presente a aula
+Anna está presente a aula
+João está presente a aula
+Maria está presente a aula
+Pedro está presente a aula
+*/
