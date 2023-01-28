337ee1b37b810b0f7d5e566f5254ef63b813efe6
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-28 17:25:54 -0300

    add my solution and teacher's solution

--- challenges_02/ex2.js
+++ challenges_02/ex2.js
@@ -1,1 +1,25 @@
 // [ ] Faça um programa que verifica se uma pessoa é brasileira ou não. // DEVE SER UM OBJETO
+
+// ***** MINHA SOLUÇÃO *****
+
+const person = {
+  nationality: "Brasil",
+};
+
+const isBrazilian =
+  person.nationality == "Brasil" ? "É brasileiro(a)" : "Não é brasileiro(a)";
+
+console.log(isBrazilian);
+
+// ***** SOLUÇÃO PROFESSOR *****
+
+const person2 = {
+  name: "João",
+  nationality: "Brasileiro",
+};
+
+if (person2.nationality === "Brasileiro") {
+  console.log("Ele é brasileiro");
+} else {
+  console.log("Ele é estrangeiro");
+}
