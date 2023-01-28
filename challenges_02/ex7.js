e1f69cc1b7770de15769a21b818009ae65e8899f
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-28 17:26:49 -0300

    add my solution and teacher's solution

--- challenges_02/ex7.js
+++ challenges_02/ex7.js
@@ -1,1 +1,15 @@
-// [ ] Faça um programa onde leia um número e diga se ele é: - ímpar, - par, - é um número primo e impar, - é par e divisível por 5.
+// [ ] Faça um programa onde leia um numero e diga se ele é par ou impar.
+
+// ***** MINHA SOLUÇÃO *****
+
+const number = 66;
+const result = number % 2 === 0 ? "Par" : "Ímpar";
+console.log(result);
+
+// ***** SOLUÇÃO PROFESSOR *****
+
+if (number % 2 === 0) {
+  console.log("Par");
+} else {
+  console.log("Ímpar");
+}
