19efbb83baa81fa192691c431c58f8c900f83ad7
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-28 09:43:45 -0300

    add content about for of

---
+++ study-material/for-of.js
@@ -0,0 +1,34 @@
+/* 
+
+ESTRUTURA DE REPETIÇÃO - FOR OF
+
+for (const iterator of object) {}
+
+*/
+
+let myName = "Bárbara";
+for (const letter of myName) {
+  console.log(letter);
+}
+
+/* RESULTADO
+B
+á
+r
+b
+a
+r
+a
+*/
+
+let allNames = ["Maria", "João", "Pedro", "Anna"];
+for (const name of allNames) {
+  console.log(name);
+}
+
+/* RESULTADO
+Maria
+João
+Pedro
+Anna
+*/
