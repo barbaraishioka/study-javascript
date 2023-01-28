6638a5e2a4c8e1b65c785824d62a2bb16e18778c
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-28 17:26:37 -0300

    add my solution and teacher's solution

--- challenges_02/ex6.js
+++ challenges_02/ex6.js
@@ -1,1 +1,58 @@
-// [ ] Faça um programa onde leia um numero e diga se ele é par ou impar.
+// [ ] Faça um programa onde os jurados vão dar a explicação para cada nota dada. São permitidas notas de 0 a 10. Cada nota deve conter uma mensagem diferente.
+
+// ***** MINHA SOLUÇÃO *****
+
+const nota = Math.round(Math.random() * 10);
+
+switch (nota) {
+  case 0:
+    console.log(`Nota ${nota}`);
+    break;
+
+  case 1:
+    console.log(`Nota ${nota}`);
+    break;
+
+  case 2:
+    console.log(`Nota ${nota}`);
+    break;
+
+  case 3:
+    console.log(`Nota ${nota}`);
+    break;
+
+  case 4:
+    console.log(`Nota ${nota}`);
+    break;
+
+  case 5:
+    console.log(`Nota ${nota}`);
+    break;
+
+  case 6:
+    console.log(`Nota ${nota}`);
+    break;
+
+  case 7:
+    console.log(`Nota ${nota}`);
+    break;
+
+  case 8:
+    console.log(`Nota ${nota}`);
+    break;
+
+  case 9:
+    console.log(`Nota ${nota}`);
+    break;
+
+  case 10:
+    console.log(`Nota ${nota}`);
+    break;
+
+  default:
+    console.log("Não foi possível verificar a nota");
+    break;
+}
+
+// ***** SOLUÇÃO PROFESSOR *****
+// igual
