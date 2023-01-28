739c4bf4e3cc842f192d7ca8619cb5f9fb613f3e
barbaraishioka <barbara.ishioka@gmail.com>
2023-01-28 17:26:25 -0300

    add my solution and teacher's solution

--- challenges_02/ex5.js
+++ challenges_02/ex5.js
@@ -1,1 +1,36 @@
-// [ ] Crie 5 objetos nesse formato{ nome: "" idade:"" sexo:"" profissão: "" nacionalidade:"" }Ao rodar o programa, deve imprimir se a pessoa é aprovada ou não no processo ,a pessoa deve ser maior de idade e brasileira para ser aprovada.Faça um programa onde os jurados vão dar a explicação para cada nota dada. São permitidas notas de 0 a 10. Cada nota deve conter uma mensagem diferente.
+// [ ] Crie 5 objetos nesse formato { nome: "" idade: "" sexo: "" profissão: "" nacionalidade: "" } Ao rodar o programa, deve imprimir se a pessoa é aprovada ou não no processo ,a pessoa deve ser maior de idade e brasileira para ser aprovada.
+
+// ***** MINHA SOLUÇÃO *****
+
+const person = {
+  nome: "Ana",
+  idade: 20,
+  sexo: "female",
+  profissão: null,
+  nacionalidade: "Brasil",
+};
+
+if (person.idade >= 18 && person.nacionalidade == "Brasil") {
+  console.log("Aprovada");
+} else {
+  console.log("Reprovada");
+}
+
+// ***** SOLUÇÃO PROFESSOR *****
+
+const personOne = {
+  nome: "Ana",
+  idade: 20,
+  sexo: "female",
+  profissão: null,
+  nacionalidade: "Brasileira",
+};
+
+if (
+  (personOne.idade >= 18 && personOne.nacionalidade == "Brasileiro") ||
+  personOne.nacionalidade == "Brasileira"
+) {
+  console.log("Você passou no processo");
+} else {
+  console.log("Você NÃO passou no processo");
+}
